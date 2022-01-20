8df5f8253529be9fc370a544f2f2a641aabbffbc
captain-tab <d491405879@gmail.com>
2022-01-21 00:17:17 +0800

    feat: build basic route map

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,19 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+import Index from './view/index'
+import About from "./view/about";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+  return <BrowserRouter>
+    <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/about" element={<About />} />
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App
